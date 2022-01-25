392c172914ce86082da0f5ed7607dd4951e6d909
sophy8281 <wanjikusophia3@gmail.com>
2022-01-25 14:50:11 +0300

    new models

--- blogproject/blogapp/models.py
+++ blogproject/blogapp/models.py
@@ -1,3 +1,26 @@
 from django.db import models
 
+
 # Create your models here.
+
+
+class Category(models.Model):
+    title = models.CharField(max_length=20)
+
+    image = models.ImageField(upload_to="category")
+
+    def __str__(self):
+        return self.title
+
+
+class Blog(models.Model):
+    title = models.CharField(max_length=20)
+    image = models.ImageField(upload_to="blogs")
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    content = models.TextField()
+    Published_date = models.DateTimeField(auto_now=True)
+    author = models.CharField(max_length=200, null=True, blank=True)
+    view_count = models.PositiveIntegerField(default=0)
+
+    def __str__(self):
+        return self.title
